eb44edda6ec1cca2cfed7e941c6c2e7cf579417e
seylorx1 <61364803+seylorx1@users.noreply.github.com>
2021-02-22 02:41:51 +0000

    Fixed library link issue.

--- js/libraries.js
+++
@@ -1,8 +0,0 @@
-function dynamicallyLoadScript(url) {
-    var script = document.createElement("script");
-    script.src = url;
-
-    document.head.appendChild(script)
-}
-
-dynamicallyLoadScript("/js/_libraries/showdown.js");
