46a887bcb124143bfd63bc29672a82536398e971
TimeForCrab <2000825@uad.ac.uk>
2021-04-19 13:52:34 +0100

    Properly fixed float volumes i hope
    pls

--- Assets/Scripts/World/Float.cs
+++ Assets/Scripts/World/Float.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Float : MonoBehaviour
 {
     public float gravityScale = 10;
+    public float moveScale = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +24,20 @@ public class Float : MonoBehaviour
         if (!other.gameObject.CompareTag("Player") || !other.gameObject.CompareTag("NoFloat"))
         {
             other.attachedRigidbody.useGravity = false;
-            other.attachedRigidbody.AddForce(transform.up * -gravityScale);
+            other.attachedRigidbody.AddForce(transform.up * gravityScale);
+            other.attachedRigidbody.AddForce(-transform.right * moveScale);
         }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+      
+        if (!other.gameObject.CompareTag("Player") || !other.gameObject.CompareTag("NoFloat"))
+        {
+            other.attachedRigidbody.AddForce(transform.up * gravityScale * Time.deltaTime);
+            other.attachedRigidbody.AddForce(-transform.right * moveScale * Time.deltaTime);
+        }
+        
     }
 
     private void OnTriggerExit(Collider other)
