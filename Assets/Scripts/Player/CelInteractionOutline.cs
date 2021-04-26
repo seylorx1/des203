08f47a18f2941cb11beb2adcd006b3b76afeb5c5
seylorx1 <61364803+seylorx1@users.noreply.github.com>
2021-04-26 14:32:32 +0100

    Added floor textures.

--- Assets/Scripts/Player/CelInteractionOutline.cs
+++ Assets/Scripts/Player/CelInteractionOutline.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CelPickupOutline : MonoBehaviour {
+public class CelInteractionOutline : MonoBehaviour {
     // Start is called before the first frame update
     public ClawController
         leftClaw,
@@ -13,14 +13,14 @@ public class CelPickupOutline : MonoBehaviour {
     }
 
     private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Grabbable") && !TransformHeld(other.transform)) {
+        if (other.gameObject.GetComponentInChildren<WorldEntity>() != null && !TransformHeld(other.transform)) {
             Renderer rend = other.GetComponent<Renderer>();
             rend.material.SetFloat("_OutlineHighlight", 1.0f);
         }
     }
 
     private void OnTriggerExit(Collider other) {
-        if (other.CompareTag("Grabbable") && !TransformHeld(other.transform)) {
+        if (other.gameObject.GetComponentInChildren<WorldEntity>() != null && !TransformHeld(other.transform)) {
             Renderer rend = other.GetComponent<Renderer>();
             rend.material.SetFloat("_OutlineHighlight", 0.0f);
         }
